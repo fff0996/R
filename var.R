ced15e4eec55f9d08212bdc47374b7685bee5832
JungHyeIn <54706837+fff0996@users.noreply.github.com>
2022-02-10 14:47:50 +0900

    Update var.R

--- var.R
+++ var.R
@@ -1,3 +1,9 @@
 aetd <- all_env_merge_testing_data %>% mutate(tile20 = ntile(PRS,20))
 
 aggregate(hyper1_normal0 ~ tile20,aetd,var)
+
+
+
+library(ggplot2)
+
+plt %>% ggplot(aes(tile10,drug_SBP)) + geom_point() + geom_line()
