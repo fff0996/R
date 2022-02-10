9a807e273492e7af9db716dba7435cf86a4b7205
JungHyeIn <54706837+fff0996@users.noreply.github.com>
2022-02-10 13:32:45 +0900

    Update var.R

--- var.R
+++ var.R
@@ -1,3 +1,3 @@
-aetd <- all_env_merge_testing_data %>% mutate(tile100 = ntile(PRS,20))
+aetd <- all_env_merge_testing_data %>% mutate(tile20 = ntile(PRS,20))
 
-aggregate(hyper1_normal0 ~ tile100,aetd,mean)
+aggregate(hyper1_normal0 ~ tile20,aetd,var)
