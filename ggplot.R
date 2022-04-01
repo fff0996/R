032fb9e5b65bdf5c5ee3e79604ab04eac37aa344
JungHyeIn <54706837+fff0996@users.noreply.github.com>
2022-04-01 16:00:10 +0900

    Update ggplot.R

--- ggplot.R
+++ ggplot.R
@@ -5,4 +5,7 @@ library(dplyr)
 
 
 rbc_var["pheno"] <- c("Red blood cell count")
-tt %>% ggplot(aes(tile10,pheno_var)) + geom_point() + geom_line() + facet_wrap(~pheno)
+tt %>% ggplot(aes(tile10,pheno_var)) + geom_point() + geom_line() + facet_wrap(~pheno)+scale_x_continuous(breaks = c(1,2,3,4,5,6,7,8,9,10))
+labs(x="Average of Max and Min Price", y="Maximum Horsepower")
+
+
