b7dc171345d3afa73559ceeb371e2d12df4e33d9
JungHyeIn <54706837+fff0996@users.noreply.github.com>
2023-08-04 17:13:00 +0900

    Update OR.R

--- OR.R
+++ OR.R
@@ -1,69 +1,32 @@
-args = commandArgs(trailingOnly=TRUE)
-
+#!/bin/usr/R
+library(data.table)
 library(dplyr)
 
+argv = commandArgs(trailingOnly =T)
+
+#data including actual value of risk factor 
+a = argv[1]
+# Disease case data
+b = argv[2]
+# risk factor PRS
+c = argv[3]
+
+a <- read.csv(paste(a,sep=""),header=T,sep="\t")
+b <- read.csv(paste(b,sep=""),header=F,sep="\t")
+c <- read.csv(paste(c,sep=""),header=T)
+names(b) <- c("eid")
+b["Disease"] <- 2
+
+m <- left_join(a,b,by="eid")
+m[is.na(m$Disease),]$Disease <- 1
+
+m2 <- left_join(m,c,by="FID")
+m2["st_pred_inf"] <- scale(m2$pred_inf)
 
 
-trait <- read.csv("/BiO/Hyein/90Traits/BT/QT_BT/OR/3%/FI.txt",sep="\t",header=F)
-trait <- trait$V1
-trait <- as.character(trait)
-vali_eid <- read.csv("/BiO/Hyein/90Traits/BT/QT_BT/HR_intersection/vali_eid.txt",sep="\t")
+mod <- glm(as.factor(Disease) ~ age + sex.x + pc1 + pc2 + pc3 + pc4 + pc5 + pc6 + pc7 + pc8 + pc9 + pc10 + array + st_pred_inf,data=m2,family=binomial)
 
 
-Dic <- args[1]
-dic <- read.csv(Dic,sep="\t")
-
-
-
-ICD10 <- dimnames(dic)[[1]]
-
-
-for (t in trait){
-
-        GPS <- read.csv(paste("/BiO/Hyein/90Traits/BT/QT_BT/2nd_validation_GWAS/44_GPS_outcome/X",sep=""))
-        GPS <- GPS[,c(1,7)]
-        GPS <- left_join(vali_eid,GPS,by="FID")
-        GPS["st_pred_inf"] <- scale(GPS$pred_inf)
-
-
-        GPS <- GPS %>% mutate(tile100 = ntile(st_pred_inf,100))
-        high_sample <- GPS[GPS$tile100 > 97,]
-        normal_sample <- GPS[GPS$tile100 > 40,]
-        normal_sample <- normal_sample[normal_sample$tile100 < 61,]
-
-        normal_sample["nor_high"] <- 1
-        high_sample["nor_high"] <- 2
-
-
-
-        normal_high_sample <- rbind(normal_sample,high_sample)
-
-        for (i in ICD10){
-                normal_high_Disease_sample <- normal_high_sample
-                eid <- unlist(dic[i,])
-                names(eid) <- NULL
-                eid <- eid[! eid %in% -9]
-                if(length(eid != 0)){
-                        case <- data.frame(eid)
-                        #names(case)[2] <- c("IID")
-                        case["disease"] <- 2
-
-        #print(case)
-                        normal_high_Disease_sample <- left_join(normal_high_Disease_sample,case,by="eid")
-                        normal_high_Disease_sample$disease[is.na(normal_high_Disease_sample$disease)] <- 1
-        #normal_high_Disease_sample <- rbind(Disease_case,Disease_control)
-                        names <- names(normal_high_Disease_sample)
-        #print(names)
-                        d <- which(names == 'disease')
-        #print(index)
-                        beta <- summary(glm(as.factor(normal_high_Disease_sample[[d]]) ~as.factor(normal_high_Disease_sample$nor_high) + normal_high_Disease_sample$array + pc1 + pc2 + pc3 + pc4 + pc5 + pc6 + pc7 + pc8 + pc9 + pc10 + age + sex ,data=normal_high_Disease_sample,family = binomial))$coef["as.factor(normal_high_Disease_sample$nor_high)2",1]
-                        se <- summary(glm(as.factor(normal_high_Disease_sample[[d]]) ~as.factor(normal_high_Disease_sample$nor_high) + normal_high_Disease_sample$array + pc1 + pc2 + pc3 + pc4 + pc5 + pc6 + pc7 + pc8 + pc9 + pc10 + age + sex ,data=normal_high_Disease_sample,family = binomial))$coef["as.factor(normal_high_Disease_sample$nor_high)2",2]
-
-                        Pvalue <- summary(glm(as.factor(normal_high_Disease_sample[[d]]) ~as.factor(normal_high_Disease_sample$nor_high) + normal_high_Disease_sample$array + pc1 + pc2 + pc3 + pc4 + pc5 + pc6 + pc7 + pc8 + pc9 + pc10 + age + sex ,data=normal_high_Disease_sample,family = binomial))$coef["as.factor(normal_high_Disease_sample$nor_high)2",4]
-                        print(paste(i,t,exp(beta),se,Pvalue))
-                }
-                else{
-                        next
-                }
-        }
-}
+beta <- summary(mod)$coef["st_pred_inf",1]
+se <- summary(mod)$coef["st_pred_inf",2]
+pvalue <- summary(mod)$coef["st_pred_inf",4]
